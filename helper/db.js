4b1b2e23a52eea24f135fc98f7a82120bed718ed
nurullah <nurullah@kalinn.nurullah>
2020-05-17 02:14:32 +0300

    mlab conf

---
+++ helper/db.js
@@ -0,0 +1,14 @@
+const mongoose  = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://movie:movie11@ds231725.mlab.com:31725/heroku_8x3n7bzr', { useNewUrlParser: true, useUnifiedTopology: true });
+
+    mongoose.connection.on('open', () => {
+        console.log("mongodb connected");
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log("mongodb error ", err);
+    });
+
+}
